e508bcdebb567f7f2f90a57fb9b557f49b54c57d
PolinaFaustova <polinamusikerin@gmail.com>
2023-01-14 10:53:25 +0100

    3polina

--- js/index.js
+++ js/index.js
@@ -1,8 +1,30 @@
 //НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"//
 
-const name = 'Vitalii';
-const age = 33;
-const hobby = 'JavaScript';
-const message = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${hobby}`; 
+// const name = 'Vitalii';
+// const age = 33;
+// const hobby = 'JavaScript';
+// const message = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${hobby}`; 
 
-console.log(message);
+// console.log(message);
+
+// ЗАПИТАТИ У КОРИСТУВАЧА ІМЯ, ПОЧТУ , ТЕЛЕФОН, І ПОКАЗАТИ У КОНСОЛІ
+// "КОРИСТУВАЧ __ ВИКОРИСТОВУЄ  __ ПОЧТУ І __ ТЕЛЕФОН"
+
+// const userName = prompt('Ваше імя');
+// const mail = prompt('Ваша пошта');
+// const mob = prompt('Ваш телефон');
+// const message = `КОРИСТУВАЧ ${userName} ВИКОРИСТОВУЄ ${mail} ПОЧТУ І ${mob} ТЕЛЕФОН`;
+
+// console.log(message);
+
+
+// ПОРАХУЙТЕ СУМУ ТОВАРІВ У КОРЗИНІ . 
+// ПОТРІБНО ВВЕСТИ СУМУ 3-Х ТОВАРІВ І ПОРАХУВАТИ ЇХ СУМУ
+
+// const mob = Number(prompt('Enter Price'));
+// const TV = Number(prompt('Enter Price'));
+// const playStation = +prompt('Enter Price');
+
+// const totalPrice = mob + TV + playStation;
+
+// console.log(totalPrice);

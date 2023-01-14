a3e6a43816b7f8825f0fc2875a50542b5f4546b7
VitalyOvsiyenko <ovsiyenko80@gmail.com>
2023-01-14 11:56:02 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,9 +10,15 @@
 // ЗАДАЧА 2
 // ЗАПИТАТИ У КОРИСТУВАЧА ІМЯ, ПОЧТУ , ТЕЛЕФОН, І ПОКАЗАТИ У КОНСОЛІ "КОРИСТУВАЧ __ ВИКОРИСТОВУЄ  __ ПОЧТУ І __ ТЕЛЕФОН"
 
-const userName = prompt('Ваше імя');
-const mail = prompt('Ваша пошта');
-const telefon = prompt('Ваш телефон')
-const message = `КОРИСТУВАЧ ${userName} ВИКОРИСТОВУЄ ${mail} ПОЧТУ І ${telefon} ТЕЛЕФОН `;
+// const userName = prompt('Ваше імя');
+// const mail = prompt('Ваша пошта');
+// const telefon = prompt('Ваш телефон')
+// const message = `КОРИСТУВАЧ ${userName} ВИКОРИСТОВУЄ ${mail} ПОЧТУ І ${telefon} ТЕЛЕФОН `;
 
-console.log(message);
+// console.log(message);
+
+
+
+
+
+

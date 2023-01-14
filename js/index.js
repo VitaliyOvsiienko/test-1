0ff628f001e074b8a397505236c56012fda318e8
VitalyOvsiyenko <ovsiyenko80@gmail.com>
2023-01-14 11:02:52 +0200

    add

---
+++ js/index.js
@@ -0,0 +1,8 @@
+//НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"//
+
+const name = 'Vitalii';
+const age = 33;
+const hobby = 'JavaScript';
+const message = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${hobby}`; 
+
+console.log(message);

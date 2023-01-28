c06eb484707608bfb504ca3c5e931a9789e305db
PolinaFaustova <polinamusikerin@gmail.com>
2023-01-28 11:10:39 +0100

    4

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -13,7 +13,6 @@
 // Классика, Рок-н-ролл
 // Рэп, Регги, Классика, Рок-н-ролл
 
-
 // const styles = ['Джаз', 'Блюз'];
 
 // styles.push('Рок-н-ролл');
@@ -28,9 +27,8 @@
 
 // console.log(styles);
 
-
 // ЗАДАЧА 2
-// Напишіть ф-цію logItems(array) , яка буде получати масив і використовуючи цикл for , який для кожного елемента масива буде виводити в консоль 
+// Напишіть ф-цію logItems(array) , яка буде получати масив і використовуючи цикл for , який для кожного елемента масива буде виводити в консоль
 // повідомлення у форматі <індекс елемента> та <значення елемента>.
 // Номерація для кожного елемента масива ['Mango', 'Poly', 'Ajax'] з індексом 0 буде виведено 1 - Mango, а для индекса 2 выведет 3 - Ajax.
 
@@ -56,14 +54,37 @@
 // }
 // }
 // console.log(smallNumber);
-const findSmallestNumber = function (numbers){
-    let smallNumber = numbers[0];
-    for(let number of numbers){
-         if (smallNumber > number){
-             smallNumber = number;
-         }
-         }
-         return findSmallestNumber;
-}
-console.log(findSmallestNumber([2, 17, 94, 1, 23, 37])); 
-console.log(findSmallestNumber([49, 4, 83, 7, 12]));
+// const findSmallestNumber = function (numbers){
+//     let smallNumber = numbers[0];
+//     for(let number of numbers){
+//          if (smallNumber > number){
+//              smallNumber = number;
+//          }
+//          }
+//          return smallNumber;
+// }
+// console.log(findSmallestNumber([2, 17, 94, 1, 23, 37]));
+// console.log(findSmallestNumber([49, 4, 83, 7, 12]));
+
+// ЗАДАЧА 4
+//  Провірте , чи являється рядок полідромом
+// const str = "ротатор"; //true
+// const newString = "Hello"; //false
+
+// const array = newString.split("");
+// array.reverse();
+// console.log(array);
+
+// const menu = array.join("");
+// console.log(newString === menu);
+
+// const string = str.split("");
+// string.reverse();
+// console.log(str);
+
+// const menu = string.join("");
+// console.log(str === menu);
+
+// const word = str.split("").reverse().join("");
+
+// console.log(word === str);

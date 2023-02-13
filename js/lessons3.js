42d5fc61bd273ca28ed3d1adce18a7f026de8bb7
VitalyOvsiyenko <ovsiyenko80@gmail.com>
2023-02-13 13:54:43 +0200

    Update lessons3.js

--- js/lessons3.js
+++ js/lessons3.js
@@ -49,93 +49,95 @@
 // 6)знайти рік народження Albert Einstein;
 // 7)визначити, чи усі вчені працювали у 19ст;
 
-const scientists = [
-  {
-    name: "Albert",
-    surname: "Einstein",
-    born: 1879,
-    dead: 1955,
-    id: 1,
-  },
-  {
-    name: "Isaac",
-    surname: "Newton",
-    born: 1643,
-    dead: 1727,
-    id: 2,
-  },
-  {
-    name: "Galileo",
-    surname: "Galilei",
-    born: 1564,
-    dead: 1642,
-    id: 3,
-  },
-  {
-    name: "Marie",
-    surname: "Curie",
-    born: 1867,
-    dead: 1934,
-    id: 4,
-  },
-  {
-    name: "Johannes",
-    surname: "Kepler",
-    born: 1571,
-    dead: 1630,
-    id: 5,
-  },
-  {
-    name: "Nicolaus",
-    surname: "Copernicus",
-    born: 1473,
-    dead: 1543,
-    id: 6,
-  },
-  {
-    name: "Max",
-    surname: "Planck",
-    born: 1858,
-    dead: 1947,
-    id: 7,
-  },
-  {
-    name: "Katherine",
-    surname: "Blodgett",
-    born: 1898,
-    dead: 1979,
-    id: 8,
-  },
-  {
-    name: "Ada",
-    surname: "Lovelace",
-    born: 1815,
-    dead: 1852,
-    id: 9,
-  },
+// const scientists = [
+//   {
+//     name: "Albert",
+//     surname: "Einstein",
+//     born: 1879,
+//     dead: 1955,
+//     id: 1,
+//   },
+//   {
+//     name: "Isaac",
+//     surname: "Newton",
+//     born: 1643,
+//     dead: 1727,
+//     id: 2,
+//   },
+//   {
+//     name: "Galileo",
+//     surname: "Galilei",
+//     born: 1564,
+//     dead: 1642,
+//     id: 3,
+//   },
+//   {
+//     name: "Marie",
+//     surname: "Curie",
+//     born: 1867,
+//     dead: 1934,
+//     id: 4,
+//   },
+//   {
+//     name: "Johannes",
+//     surname: "Kepler",
+//     born: 1571,
+//     dead: 1630,
+//     id: 5,
+//   },
+//   {
+//     name: "Nicolaus",
+//     surname: "Copernicus",
+//     born: 1473,
+//     dead: 1543,
+//     id: 6,
+//   },
+//   {
+//     name: "Max",
+//     surname: "Planck",
+//     born: 1858,
+//     dead: 1947,
+//     id: 7,
+//   },
+//   {
+//     name: "Katherine",
+//     surname: "Blodgett",
+//     born: 1898,
+//     dead: 1979,
+//     id: 8,
+//   },
+//   {
+//     name: "Ada",
+//     surname: "Lovelace",
+//     born: 1815,
+//     dead: 1852,
+//     id: 9,
+//   },
 
-  {
-    name: "Lise",
-    surname: "Meitner",
-    born: 1878,
-    dead: 1968,
-    id: 11,
-  },
-  {
-    name: "Sarah E.",
-    surname: "Goode",
-    born: 1855,
-    dead: 1905,
-    id: 10,
-  },
-  {
-    name: "Hanna",
-    surname: "Hammarström",
-    born: 1829,
-    dead: 1909,
-    id: 12,
-  },
-];
+//   {
+//     name: "Lise",
+//     surname: "Meitner",
+//     born: 1878,
+//     dead: 1968,
+//     id: 11,
+//   },
+//   {
+//     name: "Sarah E.",
+//     surname: "Goode",
+//     born: 1855,
+//     dead: 1905,
+//     id: 10,
+//   },
+//   {
+//     name: "Hanna",
+//     surname: "Hammarström",
+//     born: 1829,
+//     dead: 1909,
+//     id: 12,
+//   },
+// ];
+
+
 // 1)получити масив вчених які народились у 19ст;
 // const centure = scientists.filter((scientist) => scientist.born >= 1800 && scientist.born < 1900).map((scientist) => `${scientist.name} ${scientist.surname}`);
 // console.log(centure)
@@ -223,15 +225,52 @@ const scientists = [
 // items.addItem("Манго");
 // items.removeItem("Ківі")
 
-// ЗАДАЧА 4
-// Напиши клас Client який створює об'єкт із властивостями login і email.
-// Оголоси приватні властивості #login і #email, доступ до яких зроби через геттер та сеттер login і email.
+
 
 // ЗАДАЧА 5
 // Виконати сортування масиву назв моніторів в алфавітному та зворотному алфавітному порядку.
 // const items = ["SAMSUNG", "LG", "ASUS", "DELL", "BENQ"];
 
-// ЗАДАЧА 5
-// Виконати сортування масиву назв моніторів в алфавітному та зворотному алфавітному порядку.
+
 // const items = ["SAMSUNG", "LG", "ASUS", "DELL", "BENQ"];
+
+// const monitorAB = [...items].sort((a, b) => a.localeCompare(b));
+
+// console.log(monitorAB);
+
+// const monitorBA = [...items].sort((a, b) => b.localeCompare(a));
+
+// console.log(monitorBA)
+
+
+// ЗАДАЧА 2
+// Напишіть клас User для створення користувача з наступними властивостями:
+// username - імя, рядок
+// age - вік, число
+// numberOfPosts - кількість постів, число
+// Клас очікує один параметр - об'єкт налаштувань з однойменними властивостями.
+// Добавте метод getInfo(), який повертає рядок:
+// User ${ імя } is ${ вік } years old and has ${ кількість постів } posts.
+
+
+// class User {
+//   userName = 'Vitalii';
+//   age = 33;
+//   numberOfPosts = 10;
+
+//   getInfo() {
+//     return `User ${this.userName} is ${this.age} years old and has ${this.numberOfPosts} posts`
+//   };
+
+// }
+
+// console.log()
+
+
+
+
+
+
+
+
 

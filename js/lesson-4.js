c86239ddd7e0a12bb34adfdbdfd32ec1e4408a7a
VitalyOvsiyenko <ovsiyenko80@gmail.com>
2023-02-25 10:52:34 +0200

    task 4

---
+++ js/lesson-4.js
@@ -0,0 +1,38 @@
+// ЗАДАЧА 1
+// Створіть карточки товару з масиву використовуючи createElement];
+  
+
+
+// import cars from "./data/cars.js";
+
+// const newCars = document.querySelector(".js-goods");
+
+// const makeProductCards = ({
+//     name,
+//     img,
+//     price,
+//     description,
+// }) => {
+//     const carsEl = document.createElement('li');
+//     const nameEl = document.createElement('h2');
+//     nameEl.textContent = name;
+//     const imgEl = document.createElement('img');
+//     imgEl.src = img;
+//     imgEl.width = 300;
+//     imgEl.alt = "picture";
+//     const priceEl = document.createElement('p');
+//     priceEl.textContent = `Ціна; ${price} грн.`;
+//     const descriptionEl = document.createElement('p');
+//     descriptionEl.textContent = description;
+
+//     carsEl.append(nameEl, imgEl, priceEl, descriptionEl);
+
+//     return carsEl;
+// }
+
+// const elements = cars.map(makeProductCards);
+
+// console.log(elements);
+
+// newCars.append(...elements);
+

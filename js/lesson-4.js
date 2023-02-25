658a260fdc0b72f6cd732a32c8b9d29e62867ac1
PolinaFaustova <polinamusikerin@gmail.com>
2023-02-25 11:03:07 +0100

    new

--- js/lesson-4.js
+++ js/lesson-4.js
@@ -1,7 +1,5 @@
 // ЗАДАЧА 1
 // Створіть карточки товару з масиву використовуючи createElement];
-  
-
 
 // import cars from "./data/cars.js";
 
@@ -41,21 +39,48 @@
 //  ВІД ЦІНИ ЗА КІЛОГРАМ І КІЛЬКОСТІ КУПЛЕНОГО ПРОДУКТА , І БУДЕ ВИВОДИТИ РЕЗУЛЬТАТ НА ЕКРАН,
 
 const refs = {
-    priceInput: document.querySelector("#price"),
-    quantityInput: document.querySelector("#quantity"),
-    totalText: document.querySelector(".total"),
-    form: document.querySelector(".form"),
-    amount: document.querySelector(".amount"),
-}
-console.log(refs)
+  priceInput: document.querySelector("#price"),
+  quantityInput: document.querySelector("#quantity"),
+  totalText: document.querySelector(".total"),
+  form: document.querySelector(".form"),
+  amount: document.querySelector(".amount"),
+  container: document.querySelector(".container"),
+};
+console.log(refs);
 
-function calculator() {
-    const price = refs.priceInput.value;
-    const quantity = refs.quantityInput.value;
-    const total = price * quantity;
-    refs.totalText.textContent = total;
-    refs.amount.textContent = quantity;
+function calculator(event) {
+  event.preventDefault();
+  const price = refs.priceInput.value;
+  const quantity = refs.quantityInput.value;
+  const total = price * quantity;
+  refs.totalText.textContent = total;
+  refs.amount.textContent = quantity;
 }
 
-window.addEventListener("DOMContentLoaded", calculator)
-refs.form.addEventListener("input", calculator)
+window.addEventListener("DOMContentLoaded", calculator);
+// refs.form.addEventListener("input", calculator)
+
+// 2.2 ПЕРЕПИШІТЬ КОД , ЩОБ ЗНАЧЕННЯ МИ ОТРИМУВАЛИ ПО КЛІЦІ НА КНОПКУ ,
+//  ПРИ НАЖАТТІ ЯКОГО БУДЕ ВАМ РАХУВАТИ ТА ВИВОДИТИ РЕЗУЛЬТАТ НА ЕКРАН
+//
+refs.form.addEventListener("submit", calculator);
+
+function amountEl() {
+  const price = refs.priceInput.value;
+  const quantity = refs.quantityInput.value;
+  refs.amount.textContent = quantity;
+}
+refs.quantityInput.addEventListener("input", amountEl);
+
+// 2.3 НАПИШІТЬ  РАНДОМНИЙ КОЛІР ДЛЯ НАШОЇ ФОРМИ КОЛЬКУЛЯТОРА
+// ЗА ДОПОМОГОЮ ІНЛАЙНОВОГО СТИЛЮ
+
+const randomRgbColor = () => {
+  const r = Math.round(Math.random() * (255 - 1) + 1);
+  const g = Math.round(Math.random() * (255 - 1) + 1);
+  const b = Math.round(Math.random() * (255 - 1) + 1);
+  return `rgb(${r},${g},${b})`;
+};
+
+const colorEl = randomRgbColor();
+refs.container.style.backgroundColor = colorEl;
